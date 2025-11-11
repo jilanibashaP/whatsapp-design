4fa38b10da37920380eab9c20ebe617c25ea65c1
jilanibashaP <jilanibashaP@users.noreply.github.com>
2025-11-11 12:45:59 +0530

    fixed the cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
+
+// Enable CORS for all domains
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
